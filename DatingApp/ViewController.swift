90a6b2ef41dfdfdde3ec157d9bfa85023415efe8
miranin <tamirlan.aubakirov@alumni.nu.edu.kz>
2023-05-31 11:54:52 +0600

    Initial Commit

---
+++ DatingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DatingApp
+//
+//  Created by Tamirlan Aubakirov on 31.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
